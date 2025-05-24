603983b1f40b7a3f5286bfc421444f99db0ba783
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-05-24 08:43:54 -0300

    chore: update dependencies to latest versions and add Swagger documentation setup

--- foodClub-preview/src/main.ts
+++ foodClub-preview/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
+
+  
+  const config = new DocumentBuilder()
+    .setTitle('Food Club')
+    .setDescription('Food Club API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+ 
   await app.listen(port);
 }
 bootstrap();
