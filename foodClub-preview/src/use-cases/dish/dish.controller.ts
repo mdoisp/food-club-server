fe535f88f675e207d3cb57a0ecbb6f63efce4838
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-05-25 16:00:41 -0300

    feat: enhance DishController with detailed Swagger documentation and implement error handling for dish operations

--- foodClub-preview/src/use-cases/dish/dish.controller.ts
+++ foodClub-preview/src/use-cases/dish/dish.controller.ts
@@ -8,8 +8,11 @@ import { DeleteDishService } from './services/delete-dish.service';
 import { Response } from 'express';
 import { DishEntityInterface } from 'src/database/interfaces/dish.interface';
 import { ListDishesService } from './services/list-dishes.service';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ListDishDtoResponse } from 'src/interfaces/http/dtos/response/listDishDtoResponse';
+import { Http404 } from 'src/interfaces/http/dtos/response/http404';
+import { CreateDishDto } from 'src/interfaces/http/dtos/request/createDishDto';
+import { Http400 } from 'src/interfaces/http/dtos/response/http400';
 
 @ApiTags('Dish API')
 @Controller('Dish')
@@ -40,14 +43,48 @@ export class DishController {
   }
 
   @Get(':id')
-  getById(@Param('id') id: string): Promise<DishInterface> {
-    const dish = this.getDishByIdService.execute(Number(id));
+  @ApiParam({
+    name: 'id',
+    description: 'ID do prato',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Consulta realizada com sucesso',
+    type: ListDishDtoResponse,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Prato não encontrado',
+    type: Http404,
+  })
+  async getById(@Param('id') id: string, @Res() res: Response): Promise<DishInterface> {
+    const dish = await this.getDishByIdService.execute(Number(id));
+    if (!dish) {
+      res.status(404).json({
+        success: false,
+        message: 'Empresa não encontrada',
+      });
+      return;
+    }
 
-    return dish;
+    res.status(200).json(dish);
   }
 
   @Post()
   @HttpCode(201)
+  @ApiBody({
+    description: 'Dados do prato a serem criados',
+    type: CreateDishDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Prato criado com sucesso',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro ao criar prato',
+    type: Http400,
+  })
   create(
     @Body() dish: DishInterface, @Res() res: Response) {
     const { dish_name, price, dish_description } = dish;
@@ -63,12 +100,79 @@ export class DishController {
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() dishData: DishInterface): Promise<DishInterface> {
-    return this.updateDishService.execute(Number(id), dishData);
+  @ApiParam({
+    name: 'id',
+    description: 'ID do prato',
+  })
+  @ApiBody({
+    description: 'Dados do prato a serem atualizados',
+    type: CreateDishDto,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Prato atualizado com sucesso',
+    type: ListDishDtoResponse,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Prato não encontrado',
+    type: Http404,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro ao atualizar prato',
+    type: Http400,
+  })
+  async update(@Param('id') id: string, @Body() dishData: DishInterface, @Res() res: Response): Promise<DishInterface> {
+    const expectedFields = ['dish_name', 'price', 'dish_description'];
+    const receivedFields = Object.keys(dishData);
+    const invalidFields = receivedFields.filter(field => !expectedFields.includes(field));
+    const dish = await this.updateDishService.execute(Number(id), dishData);
+
+    if (invalidFields.length > 0) {
+      res.status(400).json({
+        success: false,
+        message: `Campos inválidos: ${invalidFields.join(', ')}`,
+      });
+      return;
+    }
+    if (!dish) {
+      res.status(404).json({
+        success: false,
+        message: 'Prato não encontrado',
+      });
+      return;
+    }
+    res.status(200).json(dish);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string): void {
+  @ApiParam({
+    name: 'id',
+    description: 'ID do prato a ser deletado',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Prato deletado com sucesso',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Prato não encontrado',
+    type: Http404,
+  })
+  async delete(@Param('id') id: string, @Res() res: Response): Promise<void> {
+  const dish = await this.getDishByIdService.execute(Number(id));
+  if (!dish) {
+      res.status(404).json({
+        success: false,
+        message: 'Prato não encontrado',
+      });
+      return;
+    }
     this.deleteDishService.execute(Number(id));
+    res.status(200).json({
+      success: true,
+      message: 'Prato deletado com sucesso',
+    });
   }
 }
