377ee1a507b64b98ddabd5978482b7b515f45b29
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-05-25 17:12:36 -0300

    feat: add CreateRestaurantDto and ListRestaurantDtoResponse with Swagger documentation; enhance RestaurantController with API responses and error handling

---
+++ foodClub-preview/src/interfaces/http/dtos/response/listRestaurantDtoResponse.ts
@@ -0,0 +1,67 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { DishInterface } from "src/use-cases/dish/dish.interface";
+
+export class ListRestaurantDtoResponse {
+    @ApiProperty({
+    type: 'number',
+    description: 'ID do restaurante',
+    example: 1,
+    })
+    id: number;
+
+    @ApiProperty({
+        type: 'string',
+        description: 'Nome do restaurante',
+        example: 'Pizzaria do João',
+    })
+    restaurant_name: string;
+
+    @ApiProperty({
+        type: 'string',
+        description: 'CNPJ do restaurante',
+        example: '12.345.678/0001-90',
+    })
+    cnpj?: string;
+
+    @ApiProperty({
+        type: 'string',
+        description: 'Rua do restaurante',
+        example: 'Rua das Flores',
+    })
+    street?: string;
+
+    @ApiProperty({
+        type: 'string',
+        description: 'Número do restaurante',
+        example: '123',
+    })
+    number?: string;
+
+    @ApiProperty({
+        type: 'string',
+        description: 'CEP do restaurante',
+        example: '12345-678',
+    })
+    zip_code?: string;
+
+    @ApiProperty({
+        type: 'string',
+        description: 'Cidade do restaurante',
+        example: 'São Paulo',
+    })
+    city?: string;
+
+    @ApiProperty({
+        type: 'string',
+        description: 'Estado do restaurante',
+        example: 'SP',
+    })
+    state?: string;
+
+    @ApiProperty({
+        type: 'array',
+        items: { type: 'object' },
+        description: 'Lista de pratos do restaurante',
+    })
+    dishes?: DishInterface[];
+}
--- foodClub-preview/src/use-cases/restaurant/restaurant.controller.ts
+++ foodClub-preview/src/use-cases/restaurant/restaurant.controller.ts
@@ -6,7 +6,13 @@ import { UpdateRestaurantService } from './services/update-restaurant.service';
 import { DeleteRestaurantService } from './services/delete-restaurant.service';
 import { Response } from 'express';
 import { RestaurantInterface } from './restaurant.interface';
+import { ApiBody, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ListRestaurantDtoResponse } from 'src/interfaces/http/dtos/response/listRestaurantDtoResponse';
+import { Http404 } from 'src/interfaces/http/dtos/response/http404';
+import { CreateRestaurantDto } from 'src/interfaces/http/dtos/request/createRestaurantDto';
+import { Http400 } from 'src/interfaces/http/dtos/response/http400';
 
+@ApiTags('Restaurant API')
 @Controller('Restaurant')
 export class RestaurantController {
   constructor(
@@ -17,14 +23,48 @@ export class RestaurantController {
   ) {}
 
   @Get(':id')
-  getById(@Param('id') id: string): Promise<RestaurantInterface> {
-    const product = this.getRestaurantByIdService.execute(Number(id));
+  @ApiParam({
+    name: 'id',
+    description: 'ID do restaurante',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Consulta realizada com sucesso',
+    type: ListRestaurantDtoResponse,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Restaurante não encontrado',
+    type: Http404,
+  })
+  async getById(@Param('id') id: string, @Res() res: Response): Promise<RestaurantInterface> {
+    const product = await this.getRestaurantByIdService.execute(Number(id));
+    if (!product) {
+      res.status(404).json({
+        success: false,
+        message: 'Empresa não encontrada',
+      });
+      return;
+    }
 
-    return product;
+    res.status(200).json(product);
   }
 
   @Post()
   @HttpCode(201)
+  @ApiBody({
+    type: CreateRestaurantDto,
+    description: 'Dados do restaurante',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Restaurante criado com sucesso',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro ao criar restaurante',
+    type: Http400,
+  })
   create(
     @Body() restaurant: RestaurantInterface, @Res() res: Response) {
     const { restaurant_name, zip_code, street, number, city, cnpj, state} = restaurant;
@@ -40,12 +80,65 @@ export class RestaurantController {
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() restaurantData: RestaurantInterface): Promise<RestaurantInterface> {
-    return this.updateRestaurantService.execute(Number(id), restaurantData);
+  @ApiParam({
+    name: 'id',
+    description: 'ID do restaurante',
+  })
+  @ApiBody({
+    description: 'Dados do restaurante a serem atualizados',
+    type: CreateRestaurantDto,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Restaurante atualizado com sucesso',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Restaurante não encontrado',
+    type: Http404,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro ao atualizar restaurante',
+    type: Http400,
+  })
+  async update(@Param('id') id: string, @Body() restaurantData: RestaurantInterface,@Res() res: Response): Promise<RestaurantInterface> {
+    const expectedFields = ['restaurant_name', 'zip_code', 'street', 'number', 'city', 'cnpj', 'state'];
+    const receivedFields = Object.keys(restaurantData);
+    const invalidFields = receivedFields.filter(field => !expectedFields.includes(field));
+    const restaurant = await this.updateRestaurantService.execute(Number(id), restaurantData);
+    if (!restaurant) {
+      res.status(404).json({
+        success: false,
+        message: 'Restaurante não encontrado',
+      });
+      return;
+    }
+    if (invalidFields.length > 0) {
+      res.status(400).json({
+        success: false,
+        message: `Os seguintes campos são inválidos: ${invalidFields.join(', ')}`,
+      });
+      return;
+    }
+    res.status(200).json(restaurant);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string): void {
+  delete(@Param('id') id: string, @Res() res: Response): void {
+    const restaurant = this.getRestaurantByIdService.execute(Number(id));
+    if (!restaurant) {
+      res.status(404).json({
+        success: false,
+        message: 'Restaurante não encontrado',
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: 'Restaurante deletado com sucesso',
+    });
+    
     this.deleteRestaurantService.execute(Number(id));
   }
 }
