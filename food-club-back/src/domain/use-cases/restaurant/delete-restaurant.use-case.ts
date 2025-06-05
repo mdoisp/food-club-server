672a011d90f37ddc65d3c074a77ce773edce1e53
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-06-05 20:12:44 -0300

    refactor: remover arquivos de configuração e entidades do projeto, incluindo .gitignore, .prettierrc, eslint.config.mjs, nest-cli.json, package.json e entidades do domínio

--- food-club-back/src/domain/use-cases/restaurant/delete-restaurant.use-case.ts
+++
@@ -1,9 +0,0 @@
-import { RestaurantRepository } from '../../repositories/restaurant.repository';
-
-export class DeleteRestaurantUseCase {
-  constructor(private readonly restaurantRepository: RestaurantRepository) {}
-
-  async execute(id: string): Promise<boolean> {
-    return this.restaurantRepository.delete(id);
-  }
-}
