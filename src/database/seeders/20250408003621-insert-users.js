3ebcc360e684111822e49a01a666e12cb3f279c8
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-06-14 13:59:19 -0300

    feat: implementar hash de senha para usuários no seeder e ajustar tipo de dados no DTO de criação de pedidos semanais

--- src/database/seeders/20250408003621-insert-users.js
+++ src/database/seeders/20250408003621-insert-users.js
@@ -1,26 +1,30 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash('password123', saltRounds);
+
     return queryInterface.bulkInsert('user', [
       {
         id: 1,
         email: 'admin@tech.com',
-        password: 'password123',
+        password: hashedPassword,
         user_type: 'restaurant',
-
       },
       {
         id: 2,
         email: 'company@tech.com',
-        password: 'password123',
+        password: hashedPassword,
         user_type: 'company',
       },
       {
         id: 3,
         email: 'employee@tech.com',
-        password: 'password123',
+        password: hashedPassword,
         user_type: 'employee',
       }
     ]);
