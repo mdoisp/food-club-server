27b38dfb173a1ed1de0d7f37d15b726b9f0127c0
Mattdias312 <matheusf.dias12@gmail.com>
2025-06-17 20:16:10 -0300

    feat: adicionar novos módulos de autenticação, empresa, prato, avaliação de prato, pedidos semanais de funcionários e restaurante, reorganizando as importações no módulo principal

--- src/interfaces/http/company.module.ts
+++ src/interfaces/http/company.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { CompanyController } from './company.controller';
-import { GetCompanyByIdService } from '../../../application/use-cases/get-company-byid.service';
-import { CreateCompanyService } from '../../../application/use-cases/create-company.service';
-import { UpdateCompanyService } from '../../../application/use-cases/update-company.service';
-import { DeleteCompanyService } from '../../../application/use-cases/delete-company.service';
+import { CompanyController } from './controllers/company.controller';
+import { GetCompanyByIdService } from '../../application/use-cases/get-company-byid.service';
+import { CreateCompanyService } from '../../application/use-cases/create-company.service';
+import { UpdateCompanyService } from '../../application/use-cases/update-company.service';
+import { DeleteCompanyService } from '../../application/use-cases/delete-company.service';
 import { DatabaseModule } from 'src/infrastructure/database/database.module';
 import { CompanyRepository } from 'src/infrastructure/database/repositories/company.repository';
-import { companyProvider } from '../../../infrastructure/providers/company.provider';
-import { ListCompaniesService } from '../../../application/use-cases/list-companies.service';
+import { companyProvider } from '../../infrastructure/providers/company.provider';
+import { ListCompaniesService } from '../../application/use-cases/list-companies.service';
 import { UserRepository } from 'src/infrastructure/database/repositories/user.repository';
 import { userProvider } from 'src/infrastructure/providers/user.provider';
 
--- src/interfaces/http/user.module.ts
+++ src/interfaces/http/user.module.ts
@@ -1,15 +1,15 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "src/infrastructure/database/database.module";
-import { UserController } from "./user.controller";
-import { CreateUserService } from "../../../application/use-cases/create-user.service";
-import { DeleteUserService } from "../../../application/use-cases/delete-user.service";
-import { GetUserByIdService } from "../../../application/use-cases/get-user-byid.service";
-import { ListUsersService } from "../../../application/use-cases/list-users.service";
-import { UpdateUserService } from "../../../application/use-cases/update-user.service";
+import { UserController } from "./controllers/user.controller";
+import { CreateUserService } from "../../application/use-cases/create-user.service";
+import { DeleteUserService } from "../../application/use-cases/delete-user.service";
+import { GetUserByIdService } from "../../application/use-cases/get-user-byid.service";
+import { ListUsersService } from "../../application/use-cases/list-users.service";
+import { UpdateUserService } from "../../application/use-cases/update-user.service";
 import { userProvider } from "src/infrastructure/providers/user.provider";
 import { UserRepository } from "src/infrastructure/database/repositories/user.repository";
-import { GetUserByEmailService } from "../../../application/use-cases/get-byemail.service";
-import { AuthService } from "../../../application/use-cases/login.service";
+import { GetUserByEmailService } from "../../application/use-cases/get-byemail.service";
+import { AuthService } from "../../application/use-cases/login.service";
 import { JwtService } from "@nestjs/jwt";
 import { AuthModule } from './auth.module';
 
