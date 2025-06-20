8e7ed894179a21e1ac41cb4a89f16a48a984bda6
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-06-20 16:18:05 -0300

    feat: adicionar seeders para usuários, restaurantes, empresas, funcionários e pratos, incluindo novos dados e imagens de perfil

--- src/infrastructure/database/seeders/20250408003622-insert-restaurant.js
+++ src/infrastructure/database/seeders/20250408003622-insert-restaurant.js
@@ -12,6 +12,51 @@ module.exports = {
         cep: '12345-678',
         number: '123',
         image: 'https://exemplo.com/imagem.jpg',
+      },
+      {
+        id: 2,
+        userId: 2,
+        name: 'Trattoria Italiana',
+        cnpj: '23.456.789/0001-01',
+        cep: '23456-789',
+        number: '456',
+        image: 'https://bomgourmet.com/bomgourmet/restaurantes/osteria-trattoria-ristorante-diferencas-locais-tipicos-italia/',
+      },
+      {
+        id: 3,
+        userId: 3,
+        name: 'Cantina Mexicana',
+        cnpj: '34.567.890/0001-12',
+        cep: '34567-890',
+        number: '789',
+        image: 'https://m.yelp.com/biz/la-cantina-mexicana-sevilla',
+      },
+      {
+        id: 4,
+        userId: 4,
+        name: 'Sushi Palace',
+        cnpj: '45.678.901/0001-23',
+        cep: '45678-901',
+        number: '101',
+        image: 'https://www.archdaily.com.br/br/906125/takeshi-temakeria-and-sushi-bar-studio-bloco-arquitetura',
+      },
+      {
+        id: 5,
+        userId: 5,
+        name: 'Green Vegan',
+        cnpj: '56.789.012/0001-34',
+        cep: '56789-012',
+        number: '202',
+        image: 'https://simonde.com.br/teva-vegetal-sao-paulo-restaurante-vegano-gastronomico-onde-carnistas-nao-sentem-falta-de-comer-animais-organicos-onde-comer/',
+      },
+      {
+        id: 6,
+        userId: 6,
+        name: 'Texas BBQ',
+        cnpj: '67.890.123/0001-45',
+        cep: '67890-123',
+        number: '303',
+        image: 'https://www.dbresciachurrascaria.com.br/unidades/guarulhos/',
       }
     ]);
   },
@@ -19,4 +64,4 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     return queryInterface.bulkDelete('restaurant', null, {});
   }
-}; 
+};
--- src/infrastructure/database/seeders/20250408003623-insert-company.js
+++ src/infrastructure/database/seeders/20250408003623-insert-company.js
@@ -6,12 +6,30 @@ module.exports = {
     return queryInterface.bulkInsert('company', [
       {
         id: 1,
-        userId: 2,
+        userId: 7,
         restaurantId: 1,
         name: 'Food Club',
         cnpj: '12.345.678/0001-90',
         cep: '12345-678',
         number: '123',
+      },
+      {
+        id: 2,
+        userId: 8,
+        restaurantId: 2,
+        name: 'Food Lovers',
+        cnpj: '98.765.432/0001-21',
+        cep: '87654-321',
+        number: '456',
+      },
+      {
+        id: 3,
+        userId: 9,
+        restaurantId: 3,
+        name: 'Gourmet Partners',
+        cnpj: '87.654.321/0001-32',
+        cep: '76543-210',
+        number: '789',
       }
     ]);
   },
@@ -19,4 +37,4 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     return queryInterface.bulkDelete('company', null, {});
   }
-}; 
+};
