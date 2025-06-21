4dde7dd19b05ad21d7c50a8fe91bb003c498a53e
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-06-21 16:37:21 -0300

    feat: adicionar suporte para atualização de imagem de perfil em restaurantes, incluindo validações e ajustes na estrutura de resposta

--- src/application/use-cases/update-restaurant.use-cases.ts
+++ src/application/use-cases/update-restaurant.use-cases.ts
@@ -1,11 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { RestaurantInterface } from '../../domain/models/restaurant.model';
 import { RestaurantRepository } from 'src/infrastructure/database/repositories/restaurant.repository';
+import { UserRepository } from 'src/infrastructure/database/repositories/user.repository';
 
 @Injectable()
 export class UpdateRestaurantService {
-  constructor(private restaurantRepository: RestaurantRepository) {}
-  execute(id: number, restaurantData: RestaurantInterface): Promise<RestaurantInterface> {
-    return this.restaurantRepository.update(id, restaurantData);
+  constructor(
+    private restaurantRepository: RestaurantRepository,
+    private userRepository: UserRepository
+  ) {}
+  async execute(id: number, restaurantData: RestaurantInterface): Promise<RestaurantInterface> {
+    if(restaurantData.profileImage){
+      const user = await this.userRepository.updateImage(restaurantData.userId, {profileImage: restaurantData.profileImage});
+      if(!user){
+        throw new BadRequestException('Usuário não encontrado');
+      }
+    }
+    const restaurant = await this.restaurantRepository.update(id, restaurantData);
+    return {
+      id: restaurant.id,
+      userId: restaurant.userId,
+      name: restaurant.name,
+      cnpj: restaurant.cnpj,
+      cep: restaurant.cep,
+      number: restaurant.number,
+      profileImage: restaurantData.profileImage
+    }
   }
 }
