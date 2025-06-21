d21752f5ef5f6f2cdfb4d8626c324ba5ef8eaaad
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-06-21 10:57:03 -0300

    feat: atualizar lógica de obtenção de pedidos semanais por funcionário para incluir novos campos e melhorar a estrutura do DTO de resposta

--- src/interfaces/http/dtos/response/employeeWeeklyOrder.dto.ts
+++ src/interfaces/http/dtos/response/employeeWeeklyOrder.dto.ts
@@ -4,6 +4,13 @@ import { OrderItemEntityInterface } from "src/domain/repositories/order-item.rep
 
 export class EmployeeWeeklyOrderResponse {
     @ApiProperty({
+        description: 'ID do pedido semanal',
+        type: Number,
+        example: 1,
+    })
+    id: number;
+
+    @ApiProperty({
         description: 'ID do funcionário',
         type: Number,
         example: 1,
@@ -17,14 +24,43 @@ export class EmployeeWeeklyOrderResponse {
     })
     dayOfWeek: DayOfWeek;
 
+    @ApiProperty({
+        description: 'ID do item do pedido',
+        type: Number,
+        example: 1,
+    })
+    orderItemId: number;
+
     createdAt?: Date;
     updatedAt?: Date;
 
     @ApiProperty({
         description: 'Informações do pedido (order)',
         type: Object,
-        example: { id: 6, dishId: 1, quantity: 1 },
+        example: { id: 1, dishId: 1, quantity: 1 },
         required: false,
     })
     order?: OrderItemEntityInterface;
+
+    @ApiProperty({
+        description: 'Informações do prato',
+        type: Object,
+        example: {
+            id: 1,
+            restaurantId: 1,
+            name: "Spaghetti Carbonara",
+            description: "A classic Italian pasta dish made with eggs, cheese, pancetta, and pepper.",
+            price: 12.99,
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLgvmB2VTV_c3jF2jr9TJqJlZunjoWldt_YA&s"
+        },
+        required: false,
+    })
+    dish?: {
+        id: number;
+        restaurantId: number;
+        name: string;
+        description: string;
+        price: number;
+        image: string;
+    };
   } 
