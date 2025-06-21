edfdf9ee27e7a09bb6986ab69ae753fc42d8e066
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-06-21 16:22:05 -0300

    feat: adicionar suporte para imagem de perfil de funcionários e implementar validações no controlador de funcionários

--- src/application/use-cases/create-employee.use-cases.ts
+++ src/application/use-cases/create-employee.use-cases.ts
@@ -1,24 +1,27 @@
 import { BadRequestException, Inject, Injectable } from "@nestjs/common";
 import { EmployeeInterface } from "../../domain/models/employee.model";
 import { EmployeeRepository } from 'src/infrastructure/database/repositories/employee.repository';
+import { UserRepository } from "src/infrastructure/database/repositories/user.repository";
 
 @Injectable()
 export class CreateEmployeeService {
     constructor(
         @Inject('EMPLOYEE_REPOSITORY')
-        private readonly employeeRepository: EmployeeRepository){}
+        private readonly employeeRepository: EmployeeRepository,
+        @Inject('USER_REPOSITORY')
+        private readonly userRepository: UserRepository
+    ){}
     async execute(employee: EmployeeInterface): Promise<void> {
         const validate = await this.validateUserCreateEmployee(employee);
         if(!validate){
             throw new BadRequestException('CPF já cadastrado');
         }
         this.employeeRepository.create(employee);
+        this.userRepository.updateImage(employee.userId, {profileImage: employee.profileImage});
     }
 
     async validateUserCreateEmployee(employee: EmployeeInterface): Promise<boolean> {
-        console.log('employee',employee);
         const employees = await this.employeeRepository.list();
-        console.log('employees',employees);
         const existingEmployee = employees.find(e => e.cpf === employee.cpf);
         if(existingEmployee){
             return false;
--- src/application/use-cases/update-employee.use-cases.ts
+++ src/application/use-cases/update-employee.use-cases.ts
@@ -1,14 +1,18 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { EmployeeInterface } from '../../domain/models/employee.model';
 import { EmployeeRepository } from 'src/infrastructure/database/repositories/employee.repository';
+import { UserRepository } from 'src/infrastructure/database/repositories/user.repository';
 
 @Injectable()
 export class UpdateEmployeeService {
   constructor(
     @Inject('EMPLOYEE_REPOSITORY')
-    private readonly employeeRepository: EmployeeRepository
+    private readonly employeeRepository: EmployeeRepository,
+    @Inject('USER_REPOSITORY')
+    private readonly userRepository: UserRepository
   ) {}
   async execute(id: number, employeeData: EmployeeInterface): Promise<EmployeeInterface> {
+    this.userRepository.updateImage(employeeData.userId, {profileImage: employeeData.profileImage});
     return await this.employeeRepository.update(id, employeeData);
   }
 }
