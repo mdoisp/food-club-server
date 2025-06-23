10bd6310c6d82d6e40d010e27fbe04a52e9552ee
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-06-23 15:03:52 -0300

    feat: adicionar variáveis de ambiente para configuração do banco de dados e atualizar dependências no Dockerfile e package.json

--- src/infrastructure/database/config.js
+++ src/infrastructure/database/config.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite'
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false, // opcional: remove logs SQL do console
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Necessário para o Render
+      }
+    }
   }
 }
